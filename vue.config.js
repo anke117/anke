960ea7829723841dca01f01caeabde70fb317ab5
anke_1170 <2287795629@qq.com>
2019-07-03 17:50:16 +0800

    修改vue.config.js配置信息

--- vue.config.js
+++ vue.config.js
@@ -31,10 +31,9 @@ module.exports = {
       // 当使用 title 选项时，
       // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
       title: "首页",
-      // 在这个页面中包含的块，默认情况下会包含
-      // 提取出来的通用 chunk 和 vendor chunk。
+      // 在这个页面中包含的块，默认情况下会包含提取出来的通用 chunk 和 vendor chunk。
       chunks: ["chunk-vendors", "chunk-common", "index"]
-    }, 
+    }
     // 当使用只有入口的字符串格式时，
     // 模板会被推导为 `public/subpage.html`
     // 并且如果找不到的话，就回退到 `public/index.html`。
@@ -65,26 +64,9 @@ module.exports = {
   //调整 webpack 配置 https://cli.vuejs.org/zh/guide/webpack.html#%E7%AE%80%E5%8D%95%E7%9A%84%E9%85%8D%E7%BD%AE%E6%96%B9%E5%BC%8F
   configureWebpack: config => {
     //生产and测试环境
-    let pluginsPro = [
-      // new CompressionPlugin({
-      //   //文件开启Gzip，也可以通过服务端(如：nginx)(https://github.com/webpack-contrib/compression-webpack-plugin)
-      //   filename: "[path].gz[query]",
-      //   algorithm: "gzip",
-      //   test: new RegExp("\\.(" + ["js", "css"].join("|") + ")$"),
-      //   threshold: 8192,
-      //   minRatio: 0.8
-      // }),
-      //	Webpack包文件分析器(https://github.com/webpack-contrib/webpack-bundle-analyzer)
-      // new BundleAnalyzerPlugin()
-    ];
+    let pluginsPro = [];
     //开发环境
-    let pluginsDev = [
-      //移动端模拟开发者工具(https://github.com/diamont1001/vconsole-webpack-plugin  https://github.com/Tencent/vConsole)
-      // new vConsolePlugin({
-      //   filter: [], // 需要过滤的入口文件
-      //   enable: true // 发布代码前记得改回 false
-      // })
-    ];
+    let pluginsDev = [];
     if (process.env.NODE_ENV === "production") {
       // 为生产环境修改配置...process.env.NODE_ENV !== 'development'
       config.plugins = [...config.plugins, ...pluginsPro];
@@ -116,28 +98,28 @@ module.exports = {
   },
   // webpack-dev-server 相关配置 https://webpack.js.org/configuration/dev-server/
   devServer: {
-    host: "localhost",
-    // host: "0.0.0.0",
+    // host: "localhost",
+    host: "192.168.50.86", //anke本地IP
     port: 8000, // 端口号
     https: false, // https:{type:Boolean}
     open: true, //配置自动启动浏览器  http://172.16.1.12:7071/rest/mcdPhoneBar/
     hotOnly: true, // 热更新
-    // proxy: 'http://localhost:8000'   // 配置跨域处理,只有一个代理
-    proxy: {
-      //配置自动启动浏览器
-      "/rest/*": {
-        target: "http://172.16.1.12:7071",
-        changeOrigin: true,
-        // ws: true,//websocket支持
-        secure: false
-      },
-      "/pbsevice/*": {
-        target: "http://172.16.1.12:2018",
-        changeOrigin: true,
-        //ws: true,//websocket支持
-        secure: false
-      }
-    }
+    proxy: null // 配置跨域处理,只有一个代理
+    // proxy: {
+    //   //配置自动启动浏览器
+    //   "/rest/*": {
+    //     target: "http://172.16.1.12:7071",
+    //     changeOrigin: true,
+    //     // ws: true,//websocket支持
+    //     secure: false
+    //   },
+    //   "/pbsevice/*": {
+    //     target: "http://172.16.1.12:2018",
+    //     changeOrigin: true,
+    //     //ws: true,//websocket支持
+    //     secure: false
+    //   }
+    // }
   },
 
   // 第三方插件配置 https://www.npmjs.com/package/vue-cli-plugin-style-resources-loader
